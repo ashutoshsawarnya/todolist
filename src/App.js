9d516dc2d4a5f30582ef79814e9178e27962bfd1
ashutoshsawarnya <atiwari54321@gmail.com>
2022-03-21 23:37:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react"
+import axios from "axios"
 
 function App() {
+  const [name,setName] = useState("")
+
+  useEffect(() =>{
+         const fetchPosts = async () =>{
+           const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+           setName(response.data)
+         }
+         fetchPosts()
+  },[])
+
+  const postData = name && name.map((test,index) =>{
+    return(
+      
+      <table border="2">
+
+        <thead>
+          <tr>
+            <td>Id:</td>
+            <td>Title:</td>
+          </tr>
+          </thead>
+          <tbody>
+                <tr key={index}>
+                  
+                  <td>{test.id}</td>
+                
+                
+                 
+                  <td>{test.title}</td>
+                </tr>
+                </tbody>
+      </table>
+      
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Posting Data from API 👍
+      {
+        name && postData
+      }
+      
     </div>
   );
 }
